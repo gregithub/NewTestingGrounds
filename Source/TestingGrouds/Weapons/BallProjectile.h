cd3ba2b53019b0cdca9c33b37805c14c0def6284
Grzegorz Skwarek <grzegorz.skwarek.95@gmail.com>
2018-05-11 21:21:37 +0200

    243.Reorganizing c++ files.

--- Source/TestingGrouds/Weapons/BallProjectile.h
+++ Source/TestingGrouds/Weapons/BallProjectile.h
@@ -4,10 +4,10 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
-#include "TestingGroudsProjectile.generated.h"
+#include "BallProjectile.generated.h"
 
 UCLASS(config=Game)
-class ATestingGroudsProjectile : public AActor
+class ABallProjectile : public AActor
 {
 	GENERATED_BODY()
 
@@ -20,7 +20,7 @@ class ATestingGroudsProjectile : public AActor
 	class UProjectileMovementComponent* ProjectileMovement;
 
 public:
-	ATestingGroudsProjectile();
+	ABallProjectile();
 
 	/** called when projectile hits something */
 	UFUNCTION()
